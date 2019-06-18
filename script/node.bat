57ef3b6350db2136f22c49ff8fcab68ebea9a756
Qui Le <quile@kms-technology.com>
2019-06-18 15:02:28 +0700

    update

--- script/node.bat
+++ script/node.bat
@@ -1,9 +1,10 @@
 @echo on
 
+::set hubURL="http://localhost:4444"
 set hubURL=""
 
 if %hubURL%=="" (
     echo "Hub URL is not configured. Please specify value of hubURL variable in the sh file"
 ) else (
-    java -jar kata-server.jar -role node -hub %hubURL%:/grid/register -proxy com.katalon.kata.proxy.KatalonProxy
+    java -jar kata-server.jar -role node -hub %hubURL%/grid/register -proxy com.katalon.kata.proxy.KatalonProxy
 )
